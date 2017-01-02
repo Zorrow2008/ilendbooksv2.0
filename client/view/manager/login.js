d89f28bf1650a76fb2839287ceddece1ebab0078
Karthikyokesh Jayakumar <Karthikyokesh Jayakumar>
2017-01-02 12:08:18 -0800

    2.0

--- client/view/manager/login.js
+++ client/view/manager/login.js
@@ -4,8 +4,13 @@ Template.login.events({
 		var email = $('[name=emailAddress]').val();
         var password = $('[name=password]').val();
          Meteor.loginWithPassword(email, password, function(){
+         	if (Meteor.user()) {
          	console.log("You initiated login process.");
+         	Router.go('/')
+         }else{
+         	Bert.alert( 'Invalid credentials.', 'danger' );
+         }
          });
-         Router.go('/')
+         
 	}
 })
